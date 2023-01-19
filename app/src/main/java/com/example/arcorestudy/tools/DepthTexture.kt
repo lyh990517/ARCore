9508d7e3ae8ce1b53a24536c61f80e8ae3f67bd6
yhlee2 <yhlee2@rsupport.com>
2023-01-19 13:25:11 +0900

    DepthTexture.kt 추가

---
+++ app/src/main/java/com/example/arcorestudy/tools/DepthTexture.kt
@@ -0,0 +1,45 @@
+package com.example.arcorestudy.tools
+
+import android.opengl.GLES30.*
+import com.google.ar.core.Frame
+import java.nio.IntBuffer
+
+class DepthTexture {
+    private var depthTextureId = -1
+    private var depthTextureWidth = -1
+    private var depthTextureHeight = -1
+
+    fun createTexture() {
+        val textureId = IntArray(1)
+        glGenTextures(1, textureId, 0)
+        depthTextureId = textureId[0]
+        glBindTexture(GL_TEXTURE_2D, depthTextureId)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    }
+
+    fun update(frame: Frame) {
+        val depthImage = frame.acquireDepthImage16Bits()
+        depthTextureWidth = depthImage.width
+        depthTextureHeight = depthImage.height
+        glBindTexture(GL_TEXTURE_2D, depthTextureId)
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RG8,
+            depthTextureWidth,
+            depthTextureHeight,
+            0,
+            GL_RG,
+            GL_UNSIGNED_BYTE,
+            depthImage.planes[0].buffer
+        )
+        depthImage.close()
+    }
+
+    fun getDepthTexture() = depthTextureId
+    fun getDepthWidth() = depthTextureWidth
+    fun getDepthHeight() = depthTextureHeight
+}
