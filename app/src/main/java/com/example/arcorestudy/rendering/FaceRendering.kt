c1121ae17b3eebef14f35ff8ba15dbba639f2a5c
yhlee2 <yhlee2@rsupport.com>
2023-02-07 15:17:17 +0900

    obj 파일 추가 및 업데이트

--- app/src/main/java/com/example/arcorestudy/rendering/FaceRendering.kt
+++ app/src/main/java/com/example/arcorestudy/rendering/FaceRendering.kt
@@ -23,17 +23,17 @@ class FaceRendering(
     private val fShader: String,
 ) {
 
-    private var faceVertex: FloatBuffer? = null
-    private var faceIndices: ShortBuffer? = null
-    private var facePos: Vec3? = null
-    private var faceUVS: FloatBuffer? = null
-    private var faceNormals: FloatBuffer? = null
+    private var faceVertex: MutableList<FloatBuffer> = mutableListOf()
+    private var faceIndices: MutableList<ShortBuffer> = mutableListOf()
+    private var facePos: MutableList<Vec3> = mutableListOf()
+    private var faceUVS: MutableList<FloatBuffer> = mutableListOf()
+    private var faceNormals: MutableList<FloatBuffer> = mutableListOf()
 
     private var facePosition = mutableListOf<Vec3>()
     private lateinit var program: Program
     private var proj = Mat4()
     private var view = Mat4()
-    private var vertexData: DataVertex? = null
+    private var vertexData: MutableList<DataVertex> = mutableListOf()
     fun init() {
         program = Program.create(vShader, fShader)
     }
@@ -42,16 +42,14 @@ class FaceRendering(
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         program.use()
-        facePos?.let {
-            vertexData?.let {
-                glBindVertexArray(it.getVaoId())
-            }
-            val model = glm.translate(Mat4(), it)
+        facePos.forEachIndexed { index, vec3 ->
+            glBindVertexArray(vertexData[index].getVaoId())
+            val model = glm.translate(Mat4(), vec3)
             program.setUniformMat4("mvp", proj * view * model)
-            GLES20.glDrawElements(GL_TRIANGLE_STRIP,faceIndices?.size ?: 0, GL_UNSIGNED_SHORT,0)
+            GLES20.glDrawElements(GL_TRIANGLE_STRIP,faceIndices[index].size, GL_UNSIGNED_SHORT,0)
             glBindVertexArray(0)
         }
-        facePos = null
+        facePos.clear()
     }
 
     fun setFace(
@@ -61,15 +59,15 @@ class FaceRendering(
         uvs: FloatBuffer,
         normals: FloatBuffer
     ) {
-        faceVertex = vertex
-        faceIndices = indices
-        facePos = pos
-        faceUVS = uvs
-        faceNormals = normals
-        vertexData = DataVertex(vertex,indices,3)
-        vertexData?.addAttribute(program.getAttributeLocation("aPos"), 3, 0)
-        vertexData?.bind()
-        Log.e("face", "${facePos}")
+        faceVertex.add(vertex)
+        faceIndices.add(indices)
+        facePos.add(pos)
+        faceUVS.add(uvs)
+        faceNormals.add(normals)
+        vertexData.add(DataVertex(vertex,indices,3).apply {
+            addAttribute(program.getAttributeLocation("aPos"), 3, 0)
+            bind()
+        })
     }
 
     fun setProjectionMatrix(projMatrix: FloatArray) {
